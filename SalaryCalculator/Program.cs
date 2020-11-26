4229f5c97695819ba0e0e6e482c688f6fc16180a
Anastasia Shimko <anastasiaishimko@gmail.com>
2020-11-26 17:37:10 +0300

    Каркас приложения, запрос необходимых данных у пользователя.

--- SalaryCalculator/Program.cs
+++ SalaryCalculator/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace SalaryCalculator
 {
-    class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Введите количество продаж, необходимое для выполнения плана: ");
+            var planSales = Console.ReadLine();
+
+            Console.WriteLine("Введите количество ваших продаж: ");
+            var ActualSales = Console.ReadLine();
+
+            var salesList = new List<Sale>();
+
+            for (int i = 1; i <= Convert.ToInt32(ActualSales); i++)
+            {
+                var sale = new Sale();
+
+                Console.WriteLine($"Введите информацию по продаже №{i}. ");
+
+                Console.WriteLine($"Введите сумму продажи №{i}: ");
+                sale.Amount = Convert.ToDouble(Console.ReadLine().Replace('.',','));
+
+                Console.WriteLine($"Введите клиента по продаже №{i}: ");
+                sale.Client = Console.ReadLine();
+
+                salesList.Add(sale);
+            }
+
+            
         }
     }
+
 }
---
+++ SalaryCalculatorBase/Models/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator
+{
+    public class Sale
+    {
+        public string Client { get; set; }
+        public double Amount { get; set; }
+    }
+}
